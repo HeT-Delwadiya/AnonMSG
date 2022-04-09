4d9fe7eb1b1a08a9cba7add0d35be2f9361a9b95
HeT-Delwadiya <hetdelwadiya@gmail.com>
2022-04-09 10:43:21 +0530

    updated css

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,29 @@
-import 'bootstrap/dist/css/bootstrap.css'
-import '../styles/globals.css'
 import Head from "next/head";
 import Script from 'next/script'
+//import 'bootstrap/dist/css/bootstrap.css'
+import '../styles/globals.css'
 import Layout from '../components/Layout';
-import { useEffect } from 'react';
+//import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
 
-       useEffect(() => {
-              typeof document !== undefined ? require('bootstrap/dist/js/bootstrap.bundle.js') : null
-       }, [])
+       // useEffect(() => {
+       //        typeof document !== undefined ? require('bootstrap/dist/js/bootstrap.bundle.js') : null
+       // }, [])
 
        return (<>
                      <Head>
                             <meta name="viewport" content="width=device-width, initial-scale=1" />
+                            <link rel="icon" href="/images/favicon.ico" />
+                            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous"/>
+
                      </Head>
                      <Script
                             src="https://kit.fontawesome.com/abd63e7fe1.js" 
                             crossorigin="anonymous"
                      />
+                     <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"/>
+
                      <Layout><Component {...pageProps} /></Layout>
               </>);
 }
