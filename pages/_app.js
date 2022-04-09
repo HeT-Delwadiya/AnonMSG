e7fa293259ab5a182bb5424bb6adc36e8a66b12b
HeT-Delwadiya <hetdelwadiya@gmail.com>
2022-04-09 10:49:59 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,20 @@
 import Head from "next/head";
 import Script from 'next/script'
-//import 'bootstrap/dist/css/bootstrap.css'
 import '../styles/globals.css'
 import Layout from '../components/Layout';
-//import { useEffect } from 'react';
 
 function MyApp({ Component, pageProps }) {
 
-       // useEffect(() => {
-       //        typeof document !== undefined ? require('bootstrap/dist/js/bootstrap.bundle.js') : null
-       // }, [])
-
        return (<>
                      <Head>
+                            <meta
+                                   name="description"
+                                   content="A plateform for developers"
+                            />
                             <meta name="viewport" content="width=device-width, initial-scale=1" />
                             <link rel="icon" href="/images/favicon.ico" />
                             <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossOrigin="anonymous"/>
-
+                            <title>AnonMSG</title>
                      </Head>
                      <Script
                             src="https://kit.fontawesome.com/abd63e7fe1.js" 
